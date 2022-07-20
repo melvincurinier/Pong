de666b25f6958f0fedf7f844c47cf8f84862d965
melvincurinier <melvin.curinier@etu.univ-lyon1.fr>
2022-07-20 14:17:22 +0100

    load screen

---
+++ main.lua
@@ -0,0 +1,20 @@
+WINDOW_WIDTH = 1600
+WINDOW_HEIGHT = 900
+
+function love.load()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+function love.draw()
+    love.graphics.printf(
+        'Hello Pong!',
+        0,
+        WINDOW_HEIGHT / 2 - 6,
+        WINDOW_WIDTH,
+        'center'
+    )
+end
